ff4507e3f438c8c85b0e5b74caa9289315343ada
violetseretse8-jpg <violetseretse8@gmail.com>
2026-09-08 11:48:16 +0200

    Script.js

---
+++ Script.js
@@ -0,0 +1,35 @@
+// My inventory
+let inventory = {
+  keyboard: [500, 5],
+  mouse: [250, 10],
+  monitor: [3000, 3]
+};
+
+// Function 1 - get details
+function getItemDetails(productName) {
+  // check if item is there
+  if (inventory[productName]) {
+    let price = inventory[productName][0];
+    let qty = inventory[productName][1];
+    return productName + " price is " + price + " qty is " + qty;
+  }
+}
+
+// Function 2 - restock
+function restockItem(productName, newQuantity) {
+  inventory[productName][1] = newQuantity;
+  return inventory;
+}
+
+// Function 3 - total value
+function getInventoryValue(productName) {
+  let price = inventory[productName][0];
+  let quantity = inventory[productName][1];
+  let total = price * quantity;
+  return total;
+}
+
+// testing
+console.log(getItemDetails("keyboard"));
+console.log(restockItem("keyboard", 10));
+console.log(getInventoryValue("keyboard"));
