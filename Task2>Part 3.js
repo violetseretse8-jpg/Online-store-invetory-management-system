ec3ee06ec06bf75e9681ee70ec1cafb140d74009
violetseretse8-jpg <violetseretse8@gmail.com>
2026-09-10 18:39:06 +0200

    Update Task2>Part 3a

--- Task2>Part 3.js
+++ Task2>Part 3.js
@@ -1,1 +1,60 @@
+// TASK 2 PART 3 - IPO
+// INPUT: developers array with nested objects
+// PROCESS: loop through developers to extract and count
+// OUTPUT: various stats about phones, setups, gadgets
 
+const developers = [
+  {
+    name: "Vee",
+    laptops: ["Dell"],
+    phones: ["Samsung", "Xiaomi"],
+    computerSetups: [{ brand: "Lenovo", monitors: 1, keyboards: 1, mice: 1, speakers: 1 }]
+  },
+  {
+    name: "Katlego",
+    laptops: ["HP", "Samsung"],
+    phones: ["Apple", "Samsung", "Tecno", "Samsung"],
+    computerSetups: [
+      { brand: "Lenovo", monitors: 2, keyboards: 1, mice: 1, speakers: 2 },
+      { brand: "Dell", monitors: 1, keyboards: 1, mice: 1, speakers: 1 }
+    ]
+  },
+  {
+    name: "Rethabile",
+    laptops: ["Samsung"],
+    phones: ["Samsung", "Huawei", "Poco"],
+    computerSetups: [
+      { brand: "Asus", monitors: 1, keyboards: 1, mice: 1, speakers: 1 },
+      { brand: "Acer", monitors: 1, keyboards: 1, mice: 1, speakers: 2 }
+    ]
+  },
+  {
+    name: "Gift",
+    laptops: [],
+    phones: ["Samsung"],
+    computerSetups: [
+      { brand: "Acer", monitors: 3, keyboards: 1, mice: 1, speakers: 2 },
+      { brand: "HP", monitors: 2, keyboards: 1, mice: 1, speakers: 2 }
+    ]
+  },
+  {
+    name: "Thokozile",
+    laptops: ["Lenovo"],
+    phones: ["Apple"],
+    computerSetups: [
+      { brand: "Dell", monitors: 1, keyboards: 1, mice: 1, speakers: 2 },
+      { brand: "Asus", monitors: 1, keyboards: 0, mice: 1, speakers: 1 },
+      { brand: "Dell", monitors: 1, keyboards: 1, mice: 1, speakers: 1 }
+    ]
+  }
+];
+
+// PART 3a - IPO
+// INPUT: developers array
+// PROCESS: for loop, get each name
+// OUTPUT: names array
+let devNames = [];
+for (let i = 0; i < developers.length; i++) {
+  devNames[devNames.length] = developers[i].name;
+}
+console.log("3a Names:", devNames);
