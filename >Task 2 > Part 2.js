a39202ad20d8433ba881b05b40e5dd9f5031cf2c
violetseretse8-jpg <violetseretse8@gmail.com>
2026-09-10 18:23:40 +0200

    Update >Task 2 > Part 2.js

--- >Task 2 > Part 2.js
+++ >Task 2 > Part 2.js
@@ -1,1 +1,7 @@
+// TASK 2 - IPO
+// INPUT: mixed array with numbers, strings, booleans
+// PROCESS: use loops to filter and process by type
+// OUTPUT: different filtered arrays and greeting
 
+let mixed = [7, 10, "Clentan", 13, 89, true, 45, false, "Jerry", "Vukona", "Reabetswe", 600];
+console.log("Original:", mixed);
